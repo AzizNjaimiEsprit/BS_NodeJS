043e442dea66ebfe8067700d2a37c4b9be0137ed
Med Aziz Njaimi <mohamedaziz.njaimi@esprit.tn>
2021-04-25 22:27:28 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+var cors = require('cors');
+const app = express();
+app.use(cors());
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+////////////////////////////////////////////////////////////////////////////
+
+require('./CommandsRouter/index')(app);
+
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=> console.log(`Port ${port} active`));
